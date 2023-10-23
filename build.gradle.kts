65cddc25df902f2492998aba9b4c1b27ce9ea11f
David Herman <bitspittle@gmail.com>
2023-10-22 21:06:26 -0700

    Stop using mavenlocal

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,6 @@ subprojects {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
-        ///// DO NOT SUBMIT
-        mavenLocal {
-            content {
-                includeGroupByRegex("com\\.varabyte\\.kobweb.*")
-            }
-        }
-//        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,15 +2,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-
-
-        ///// DO NOT SUBMIT
-        mavenLocal {
-            content {
-                includeGroupByRegex("com\\.varabyte\\.kobweb.*")
-            }
-        }
-//        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
 }
 
